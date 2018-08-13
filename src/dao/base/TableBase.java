361db25fb0ab198e650d6cce46484baea9e1e3ee
koryo <koryovip@yahoo.co.jp>
2018-08-14 01:55:45 +0900

    mod

--- src/dao/base/TableBase.java
+++ src/dao/base/TableBase.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public abstract class TableBase<S extends TableBase, T extends ColBase<?>> {
+public abstract class TableBase<S extends TableBase<?, ?>, T extends ColBase<?>> {
 
     public abstract String tableName();
 
