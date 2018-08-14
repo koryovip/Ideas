5ebcc0e195a32e4ee7493b255398b87a9897eb70
kou-ry <kou-ry@PC-00121.local.growtech.co.jp>
2018-08-14 14:26:23 +0900

    add

--- src/dao/base/ColBase.java
+++ src/dao/base/ColBase.java
@@ -1,37 +1,30 @@
 package dao.base;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 public abstract class ColBase<T> {
 
-    public abstract String name();
+    private final String name;
 
-    //    abstract public ColBase<T> eq(T value);
+    public ColBase(String name) {
+        this.name = name;
+    }
 
-    //    @SuppressWarnings("unchecked")
-    //    public Class<T> getType() {
-    //        // 実行時の型が取れる。ここではHogeDaoなど
-    //        Class<?> clazz = this.getClass();
-    //        // ここではBaseDao<Hoge>がとれる
-    //        Type type = clazz.getGenericSuperclass();
-    //        ParameterizedType pt = (ParameterizedType) type;
-    //        // BaseDaoの型変数に対するバインドされた型がとれる
-    //        Type[] actualTypeArguments = pt.getActualTypeArguments();
-    //        /*Class<?> entityClass =*/ return (Class<T>) actualTypeArguments[0];
-    //        // リフレクションでインスタンスを生成
-    //        //  return (T) entityClass.newInstance();
-    //    }
-    //
-    //    @SuppressWarnings("unchecked")
-    //    public T getType2() throws InstantiationException, IllegalAccessException {
-    //        // 実行時の型が取れる。ここではHogeDaoなど
-    //        Class<?> clazz = this.getClass();
-    //        // ここではBaseDao<Hoge>がとれる
-    //        Type type = clazz.getGenericSuperclass();
-    //        ParameterizedType pt = (ParameterizedType) type;
-    //        // BaseDaoの型変数に対するバインドされた型がとれる
-    //        Type[] actualTypeArguments = pt.getActualTypeArguments();
-    //        Class<?> entityClass = (Class<T>) actualTypeArguments[0];
-    //        // リフレクションでインスタンスを生成
-    //        return (T) entityClass.newInstance();
-    //    }
+    final public String name() {
+        return this.name;
+    }
+
+    public Class<T> getType() {
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+        Type[] actualTypeArguments = genericSuperclass.getActualTypeArguments();
+        for (Type type : actualTypeArguments) {
+            type.getTypeName();
+            System.out.println(type);
+        }
+        @SuppressWarnings("unchecked")
+        Class<T> tClass = (Class<T>) actualTypeArguments[0];
+        return tClass;
+    }
 
 }
--- src/dao/def/T_BOOK_COL.java
+++ src/dao/def/T_BOOK_COL.java
@@ -4,18 +4,12 @@ import dao.base.ColBase;
 
 public class T_BOOK_COL<T> extends ColBase<T> {
 
-    private final String name;
-
     public T_BOOK_COL(String name) {
-        this.name = name;
+        super(name);
     }
 
-    @Override
-    public String name() {
-        return this.name;
-    }
-
-    public static final T_BOOK_COL<String> ID = new T_BOOK_COL<String>("ID");
+    public static final T_BOOK_COL<String> ID = new T_BOOK_COL<String>("ID") {
+    };
 
     //    @Override
     //    public T_BOOK_COL<T> eq(T value) {
