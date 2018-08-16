4feeca6ba4f93b84553b0efe01becb9b39a67bbc
kou-ry <kou-ry@PC-00121.local.growtech.co.jp>
2018-08-16 12:53:34 +0900

    fix bugs

--- src/dao2/def/T_USER.java
+++ src/dao2/def/T_USER.java
@@ -6,33 +6,41 @@ import java.util.List;
 
 import dao2.base.TblBase;
 
-public class T_USER extends TblBase<T_USER_COL<?>> {
+public final class T_USER extends TblBase<T_USER_COL<?>> {
 
     public T_USER() {
         super("T_USER");
     }
 
-    final public static T_USER $() {
+    public final static T_USER $() {
         return new T_USER();
     }
 
     private List<T_USER_COL<?>> columns = null;
 
+    private final Byte lock = 0;
+
     @Override
-    public List<T_USER_COL<?>> columnAll() {
+    public final List<T_USER_COL<?>> columnAll() {
         if (columns == null) {
-            columns = new ArrayList<T_USER_COL<?>>();
+            synchronized (this.lock) {
+                if (columns == null) {
+                    List<T_USER_COL<?>> tmp = new ArrayList<T_USER_COL<?>>(this.colCount);
+                    tmp.add(T_USER_COL.USER_ID);
+                    tmp.add(T_USER_COL.SCORE);
+                    tmp.add(T_USER_COL.REG_DT);
+                    columns = tmp;
+                }
+            }
         }
-        columns.add(T_USER_COL.USER_ID);
-        columns.add(T_USER_COL.SCORE);
-        columns.add(T_USER_COL.REG_DT);
         return columns;
     }
 
-    final public String selectByPrimaryKey(String userId, Date registeDate) {
+    public final String selectByPrimaryKey(String userId, Date registeDate) {
         T_USER_COL.USER_ID.where(this, userId);
         T_USER_COL.REG_DT.where(this, registeDate);
         return super.select(T_USER_COL.USER_ID);
     }
 
+    private final int colCount = 3;
 }
